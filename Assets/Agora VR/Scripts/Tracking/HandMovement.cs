fe839c416b1e36e5996a6e66d8679dcde943fe8e
DanNguyen-CE <43036823+DanNguyen-CE@users.noreply.github.com>
2020-03-03 19:33:44 -0500

    Add basic hand and head tracking
    Co-Authored-By: Michael Truong <mt842@scarletmail.rutgers.edu>

---
+++ Assets/Agora VR/Scripts/Tracking/HandMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour
+{
+    Vector3 lastPosition;
+    float distanceTravelled;
+    float seconds;
+    public Transform body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosition = transform.position;
+        distanceTravelled = 0.0f;
+        seconds = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float oldDistance = distanceTravelled;
+        distanceTravelled += Vector3.Distance(transform.position, lastPosition);
+        lastPosition = transform.position;
+
+        if (distanceTravelled - oldDistance > 0.02f)
+        {
+            seconds += Time.deltaTime;
+            Debug.Log(seconds);
+        }
+    }
+}
